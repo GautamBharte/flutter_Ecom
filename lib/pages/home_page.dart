d701a078fe0697b972da2dc8d83a82ee60391153
Gautam Kailas Bharte <gautambharte123@gmail.com>
2022-12-17 18:11:37 +0530

    created models

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,19 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
+
+import '../widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   int days = 30;
   String name = "Gautam Bharte";
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(20, (index) => CatalogModel().items[0]);
     return Scaffold(
       appBar: AppBar(
         // titleTextStyle: TextStyle(color: Colors.black),
         title: Text("Catalog App"),
       ),
-      body: Center(
-        child: Container(
-          child: Text("Welcome to $days days of flutter by $name"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          },
         ),
       ),
       drawer: MyDrawer(),
