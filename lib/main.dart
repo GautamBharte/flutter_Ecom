2c830d62ab1585c3c61ac3f1bb16d04b1293c9e2
Gautam Kailas Bharte <gautambharte123@gmail.com>
2022-12-19 08:45:17 +0530

    CartPage Structure

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       // home: HomePage(),
-      themeMode: ThemeMode.dark,
+      themeMode: ThemeMode.light,
       theme: MyTheme.lightTheme(context),
       darkTheme: MyTheme.darkTheme(context),
       debugShowCheckedModeBanner: false,
